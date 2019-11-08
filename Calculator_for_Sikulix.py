6201d4ab7951b6e367fe3be528e802d5aa950571
Xavi Rambla <45351293+xavirambla@users.noreply.github.com>
2019-11-08 13:12:57 +0100

    Update Calculator_for_Sikulix.py

--- Calculator_for_Sikulix.py
+++ Calculator_for_Sikulix.py
@@ -3,37 +3,20 @@ operando1 = input("Inserte el primer operando")
 operando2 = input("Inserte el segundo operando")
 operacion = input(u"Inserte la operación")
 
-#Tratar primer operando
-if operando1=="0":     click("1572955029914.png")
-elif operando1=="1":   click("1572955036597.png")
-elif operando1=="2":   click("1572955053174.png")
-elif operando1=="3":   click("1572955043596.png")
-elif operando1=="4":   click("1572955060741.png")
-elif operando1=="5":   click("1572955068421.png")
-elif operando1=="6":   click("1572955074469.png")
-elif operando1=="7":   click("1572955089214.png")
-elif operando1=="8":   click("1572955080982.png")
-elif operando1=="9":   click("1572955095564.png")
-#Tratar Operación
-if operacion=="+":   click("1572955279952.png")
-else:
-     if operacion=="-":        click("1572955289482.png")
-     else:
-         if operacion=="*":    click("1572955298923.png")
-        else:
-            if operacion=="/":    click("1572955308197.png")
-		 
-#Tratar segundo operando
-if operando2=="0":     click("1572955029914.png")
-elif operando2=="1":   click("1572955036597.png")
-elif operando2=="2":   click("1572955053174.png")
-elif operando2=="3":   click("1572955043596.png")
-elif operando2=="4":   click("1572955060741.png")
-elif operando2=="5":   click("1572955068421.png")
-elif operando2=="6":   click("1572955074469.png")
-elif operando2=="7":   click("1572955089214.png")
-elif operando2=="8":   click("1572955080982.png")
-elif operando2=="9":   click("1572955095564.png")
-#Resultado
+def click_numero(numero):
+    listaNums=["1572955029914.png","1572955036597.png","1572955053174.png","1572955043596.png","1572955060741.png","1572955068421.png","1572955074469.png","1572955089214.png","1572955080982.png","1572955095564.png"]
+    click(listaNums[int(numero)])
+
+def click_operacion(operacion):
+    listaOperaciones ={
+            '+':   "1572955279952.png", '-':   "1572955289482.png",  '*':   "1572955298923.png", '/':   "1572955308197.png",
+            }
+    click(listaOperaciones[operacion])
+
+click_numero(operando1)
+click_operacion(operacion)
+click_numero(operando2)
+
 type(Key.ENTER)
     
+
